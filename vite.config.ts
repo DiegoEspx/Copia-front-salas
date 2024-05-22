7b30874850986b60b870aa33a5f2602874189604
diego <diego.guerreroe@campusucc.edu.co>
2024-05-21 20:42:54 -0500

    sadsssss

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Configura la base URL
+  server: {
+    port: 3000, // Configura el puerto de desarrollo si es necesario
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Configuración de la carpeta de salida
+    sourcemap: true, // Incluye sourcemaps si necesitas para depuración
+  },
 })
